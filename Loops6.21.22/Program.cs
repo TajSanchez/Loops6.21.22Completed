7936dc6f1e20f692df9c74a2c27b0ddd3d658c92
Taj <tajmarie92@gmail.com>
2022-06-21 21:20:32 -0500

    Add project files.

---
+++ Loops6.21.22/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Loops6._21._22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //**Assignment 1**//
+
+            //int num = int.Parse(Console.ReadLine());
+            //bool isPrime = true;
+            //for (int i = 2; i <= Math.Sqrt(num); i++)
+            //{
+            //    if (num % i == 0)
+            //    {
+            //        isPrime = false;
+            //        break;
+
+            //    }
+
+            //}
+
+            //if (isPrime)
+            //    Console.WriteLine("Number is PRIME");
+            //else
+            //    Console.WriteLine("Number is NOT PRIME");
+
+            //**Assignment 2**//
+
+            ////For Loop//
+            //for (int i = 0; i <10; i++)
+            //{
+            //    Console.WriteLine("Loop nr" + i);
+            //}
+
+            ////Foreach//
+            //string[] names = new string[4] { "Daniel", "John", "Jane", "Lisa" };
+
+            //foreach (string name in names)
+            //{
+            //    Console.WriteLine(name);
+            //}
+
+            ////While//
+
+            //int num01 = 0;
+
+            //while (num01 < 10)
+            //{
+            //    Console.WriteLine("Loop nr" + num01);
+            //    num01++;
+            //}
+
+            ////Do While//
+            //int num02 = 0;
+
+            //do
+            //{
+            //    Console.WriteLine("Loop nr" + num02);
+            //    num02++;
+            //} while (num02 < 10);
+
+
+            //**Assignment 3**//
+
+            //int i = 1;
+            //while (i <= 5)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+            //Console.ReadLine();
+
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.ReadLine();
+        }
+    }
+}
